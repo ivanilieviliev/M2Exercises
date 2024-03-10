a4a9ac4e2a0e9f0e1f91ace761cec85abe6ec5f1
ivanilieviliev <ivaniliev@schoolmath.eu>
2024-03-10 11:47:06 +0200

    Added Bombs and SumOfBigNumbers Tasks

---
+++ Bombs/Bombs/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bombs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			try
+			{
+                List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+                List<int> bombAndPower = Console.ReadLine().Split().Select(int.Parse).ToList();
+                int bomb = bombAndPower[0];
+                int power = bombAndPower[1];
+
+                int elementsToRemove;
+                for (int i = 0; i < numbers.Count && i >= 0;) // i >= 0 for case like 2 3 2 2 and 3 2
+                {
+                    if (numbers[i] == bomb)
+                    {
+                        elementsToRemove = power * 2 + 1;
+
+                        for (int j = i - power; j <= i + power;)
+                        {
+                            if (j < 0)
+                            {
+                                j++;
+                                elementsToRemove--;
+                                continue;
+                            }
+
+                            if (j == numbers.Count)
+                            {
+                                break;
+                            }
+
+                            if (elementsToRemove == 0)
+                            {
+                                break;
+                            }
+
+                            numbers.RemoveAt(j);
+                            elementsToRemove--;
+                        }
+
+                        i = i - power;
+                    }
+                    else
+                    {
+                        i++;
+                    }
+                }
+
+                Console.WriteLine(numbers.Sum());
+            }
+            catch (Exception ex)
+			{
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ SumOfBigNumbers/SumOfBigNumbers/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace SumOfBigNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string number1 = Console.ReadLine();
+            string number2 = Console.ReadLine();
+
+            if (number1.Length < number2.Length)
+            {
+                number1 = ResizeString(number1, number2.Length - number1.Length);
+            }
+            else if(number1.Length > number2.Length)
+            {
+                number2 = ResizeString(number2, number1.Length - number2.Length);
+            }
+
+            StringBuilder result = new StringBuilder(new string('0', number1.Length));
+            byte sum = 0;
+
+            for (int i = number1.Length - 1; i >= 0; i--)
+            {
+                sum = (byte)((Convert.ToByte(number1[i].ToString()) + Convert.ToByte(number2[i].ToString())) + sum);
+
+                if (sum >= 10)
+                {
+                    sum = (byte)(sum % 10);
+                    result[i] = Convert.ToChar(sum.ToString());
+                    sum = 1;
+                }
+                else
+                {
+                    result[i] = Convert.ToChar(sum.ToString());
+                    sum = 0;
+                }
+            }
+
+            if (sum == 1)
+            {
+                result.Insert(0, '1');
+            }
+
+            Console.WriteLine(result.ToString());
+        }
+
+        static string ResizeString(string number, int newLength)
+        {
+            // return number.Insert(0, new string('0', newLength).ToCharArray().ToList());
+            return number.Insert(0, new string('0', newLength));
+        }
+
+    }
+}
